c71b9e6ca419bad44b47cb1a6bcb054337e9a375
augustoam <augusto.albertoni@gmail.com>
2017-09-03 22:35:59 -0300

    First

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,4 @@
 class Usuario < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,11 +33,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: ENV["GMAIL_DOMAIN"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "augusto.albertoni.teste@gmail.com",
-    password: "h!fir3!16021602"
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
 
   # Print deprecation notices to the Rails logger.
