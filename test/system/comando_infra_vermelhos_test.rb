5287cdc63234991e28baf78570cc3c538d1d3c16
augustoam <augusto.albertoni@gmail.com>
2017-09-29 22:00:37 -0300

    Controle televisão

---
+++ test/system/comando_infra_vermelhos_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ComandoInfraVermelhosTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit comando_infra_vermelhos_url
+  #
+  #   assert_selector "h1", text: "ComandoInfraVermelho"
+  # end
+end
