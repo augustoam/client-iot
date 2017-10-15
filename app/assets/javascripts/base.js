5263d6314f90bd725d11cb37b44e5c39935e7a57
Unknown <jpm_rs@hotmail.com>
2017-10-15 14:41:03 -0200

    Numeric Keyboard

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -8,10 +8,17 @@ $(document).on('turbolinks:load', function() {
   $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+
+    // Start material select box
+    $('select').material_select();
   });
 
   $('#msg-close').on('click', function(){
     document.getElementById("msg-hide").style.display = "none";
+  });
+
+  $('#numeric-keyboard').on('focus', function(){
+    document.getElementById("numeric-keyboard").value = "";
   });
 
   $(".button-collapse").sideNav({
@@ -26,10 +33,6 @@ $(document).on('turbolinks:load', function() {
       belowOrigin: true
     }
   );
-
-  $(document).ready(function() {
-    $('select').material_select();
-  });
 
   $('.notice').each(notice);
 
