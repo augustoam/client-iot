cedae8cee5671c79aa1ad4bb4d715856619b3a84
augustoam <augusto.albertoni@gmail.com>
2017-10-25 21:38:33 -0200

    rack-cors

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,14 @@ module AugustoClientWeb
     config.i18n.available_locales = ["pt-BR", "en"]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    #config.load_defaults 5.1
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
