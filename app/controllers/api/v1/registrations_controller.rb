2beb82d3fb77b17fd93f64f93b2a753759f7347b
augustoam <augusto.albertoni@gmail.com>
2018-09-01 00:35:41 -0300

    Ajustes

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,3 +1,23 @@
 class Api::V1::RegistrationsController < Api::V1::BaseController
-
+  def registration_new
+    usuario = Usuario.new(
+      email: JSON.parse(params[:email]),
+      password: JSON.parse(params[:password]),
+      password_confirmation: JSON.parse(params[:password])
+    )
+    if usuario.valid?
+      usuario.save
+      usuario.confirm
+      token_notificacao = JSON.parse(params[:token_notificacao])
+      unless usuario.tokens_notificacao_mobile.find_by(token: token_notificacao)
+        if token_notificacao != 'undefined'
+          usuario.tokens_notificacao_mobile.create!(token: token_notificacao)
+        end
+      end
+      debugger
+      render json: { usuario: usuario.email, token: usuario.token }, status: :ok
+    else
+      render json: { msg: 'Este email já esta cadastrado!' }, status: :unauthorized
+    end
+  end
 end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,23 +1,15 @@
 class Api::V1::SessionsController < Api::V1::BaseController
   def session_new
-    usuario = Usuario.find_by_email(params['email'])
+    usuario = Usuario.find_by_email(JSON.parse(params['email']))
     if usuario.present?
-      if usuario.valid_password?(params['password'])
-        if usuario.confirmed?
-          # Cria o token para notificação para o dispositivo do cliente
-          unless usuario.tokens_notificacao_mobile.find_by(token: params[:token_notificacao])
-            if params[:token_notificacao] != 'undefined'
-              usuario.tokens_notificacao_mobile.create!(token: params[:token_notificacao])
-            end
-          end
-          sign_in :Usuario, usuario
-          render json: { usuario: usuario.email, token: usuario.token }, status: :ok
-        else
-          render json: { msg: 'Email não confirmado!' }, status: :unauthorized
+      token_notificacao = JSON.parse(params[:token_notificacao])
+      # Cria o token para notificação para o dispositivo do cliente
+      unless usuario.tokens_notificacao_mobile.find_by(token: token_notificacao)
+        if token_notificacao != 'undefined'
+          usuario.tokens_notificacao_mobile.create!(token: token_notificacao)
         end
-      else
-        render json: { msg: 'Senha inválida!' }, status: :unauthorized
       end
+      render json: { usuario: usuario.email, token: usuario.token }, status: :ok
     else
       render json: { msg: 'Email inválido!' }, status: :unauthorized
     end
