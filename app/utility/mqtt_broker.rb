a74e9fad6e865a923ecc80dbe65fb5489ba27c27
augustoam <augusto.albertoni@gmail.com>
2018-09-05 00:28:01 -0300

    Ajustes no job mqtt

--- app/utility/mqtt_broker.rb
+++ app/utility/mqtt_broker.rb
@@ -3,8 +3,16 @@ module MqttBroker
   require 'mqtt'
 
   def publish_mqtt(topico, state)
-    MQTT::Client.connect('iot.eclipse.org', 1883) do |client|
-      client.publish(topico, state)
-    end
+    uri = URI.parse ENV['CLOUDMQTT_URL']
+      conn_opts = {
+        remote_host: uri.host,
+        remote_port: uri.port,
+        username: uri.user,
+        password: uri.password,
+      }
+
+      MQTT::Client.connect(conn_opts) do |_client|
+        _client.publish(topico, state)
+      end
   end
 end
