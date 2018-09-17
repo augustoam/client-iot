a93de73d2b3ba4a17f13183878a199d29e42348b
augustoam <augusto.albertoni@gmail.com>
2018-09-16 23:24:06 -0300

    Ajustes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   Rails.application.routes.default_url_options[:host] = 'client-iot.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
