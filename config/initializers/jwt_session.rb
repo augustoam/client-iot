bbfa0dae76a4725417ce488549cda6f9f774b451
jeancaio <jeanandreolla@hotmail.com>
2019-03-21 19:51:53 -0300

    aumentado o tempo de expiracao do jwtsessions

--- config/initializers/jwt_session.rb
+++ config/initializers/jwt_session.rb
@@ -1,5 +1,7 @@
 JWTSessions.algorithm = 'HS256'
 JWTSessions.encryption_key = Rails.application.secrets.secret_jwt_encryption_key
+JWTSessions.access_exp_time  = 604_800
+JWTSessions.refresh_exp_time = 604_800
 JWTSessions.token_store = :redis, {
   redis_host: 'localhost',
   redis_port: '6379',
