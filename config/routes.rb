2a85ba936c399b4b55ea52e09a02a4c714348d67
augusto albertoni <augusto.albertoni@urbanasolutions.it>
2020-01-21 22:55:04 +0100

    Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -25,71 +25,6 @@ Rails.application.routes.draw do
     post :registration_new
   end
 
-  resources :groups do
-    resources :users
-    resources :group_rooms do
-      resources :room_devices, shallow: true do
-        resources :control_layouts, shallow: true
-      end
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      resources :sessions do
-        post :reset_password, on: :collection
-      end
-      resources :registrations
-      namespace :users_group do
-        post :add_user_group
-        post :destroy_user_group
-        post :set_admin_user_group
-      end
-      namespace :mqtt do
-        post :publish
-      end
-      namespace :groups do
-        get :get_groups
-        post :new_group
-        post :edit_group
-        post :destroy_group
-      end
-      namespace :rooms do
-        post :new_room
-        post :edit_room
-        post :destroy_room
-      end
-      namespace :room_devices do
-        get :get_room_devices
-        post :new_room_device
-        post :edit_room_device
-        post :destroy_room_device
-      end
-    end
-    namespace :request do
-      post :google_api_request
-    end
-    namespace :devices do
-      post :publish
-      post :recebe_estado
-    end
-  end
-
-  namespace :api do
-    namespace :v2 do
-      resources :sessions do
-        post :reset_password, on: :collection
-      end
-      resources :registrations
-      namespace :groups do
-        get :get_groups
-        post :new_group
-        post :edit_group
-        post :destroy_group
-      end
-    end
-  end
-
   resources :users do
     get :password_change
     patch :password_update
@@ -125,13 +60,6 @@ Rails.application.routes.draw do
 
   namespace :root do
     get :index
-  end
-
-  namespace :home do
-    get :console
-    get :portfolio
-    post :mode_on
-    post :mode_off
   end
 
   require 'sidekiq/web'
