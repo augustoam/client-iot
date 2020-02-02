45756c9a96ae803a15a37996a1124a98bad779bd
augusto albertoni <augusto.albertoni@urbanasolutions.it>
2020-02-02 19:33:31 +0100

    Get groups

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -5,40 +5,40 @@ class Api::V1::GroupsController < Api::V1::BaseController
     response = []
     user_session.user_groups.collect { |group_user|
       result = {}
-      result[:group]       = group_user.group
-      result[:group_rooms] = group_user.group.group_rooms.collect do |group_room|
+      result[:group]      = group_user.group
+      result[:groupRooms] = group_user.group.group_rooms.collect do |group_room|
         data = {}
         devices = {}
-        data[:group_room] = {
-          group_room: group_room,
-          room_devices:  group_room.room_devices.collect do |room_device|
-            devices[:room_device] = {
-              room_device: room_device,
+        data[:groupRoom] = {
+          groupRoom: group_room,
+          roomDevices:  group_room.room_devices.collect do |room_device|
+            devices[:roomDevice] = {
+              roomDevice: room_device,
               control: room_device.control,
               device: room_device.control.device
             }
           end
         }
       end
-      result[:group_automations] = group_user.group.group_automations.collect do |group_automation|
+      result[:groupAutomations] = group_user.group.group_automations.collect do |group_automation|
         data = {}
         group_automation_condition = {}
-        data[:group_automation] = {
-          group_automation: group_automation,
-          group_automation_condicoes: group_automation.group_automation_conditions do |group_automation_condition|
-            group_automation_condition[:group_automation_condition] = {
-              group_automation_condition: group_automation_condition
+        data[:groupAutomation] = {
+          groupAutomation: group_automation,
+          groupAutomationCondicoes: group_automation.group_automation_conditions do |group_automation_condition|
+            group_automation_condition[:groupAutomationCondition] = {
+              groupAutomationCondition: group_automation_condition
             }
           end
         }
       end
-      result[:rooms]       = Room.all.order(created_at: :asc)
-      result[:devices]     = Device.all.order(created_at: :asc)
-      result[:controls]    = Control.where(device: syncinfra)
-      result[:user_groups] = group_user.group.users.select('user_groups.*', 'users.email').order(created_at: :asc)
+      result[:rooms]      = Room.all.order(created_at: :asc)
+      result[:devices]    = Device.all.order(created_at: :asc)
+      result[:controls]   = Control.where(device: syncinfra)
+      result[:userGroups] = group_user.group.users.select('user_groups.*', 'users.email').order(created_at: :asc)
       response << result
     }
-    debugger
+
     render json: response, status: :ok
   rescue => exception
     render json: { msg: 'Ops.. parece que aconteceu um problema =(', err: exception }, status: :unauthorized
